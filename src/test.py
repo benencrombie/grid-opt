80cf3fe4df1911d33514daaf0f4750257066e659
bcrombie <bcrombie09@gmail.com>
2025-06-27 09:27:00 -0400

    Refactored package and object names

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import json
-with open('data/locations.json', 'r') as file:
-    data = json.load(file)['Profile']
-    
-
-for i in data.keys():
-    print(i)
-# base_locations = [(data_dict[loc["x_coord"]], data_dict[loc["y_coord"]], data_dict[loc["weight"]]) for loc in data_dict]
-# print(base_locations)
