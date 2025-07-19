7f3eb513ee4de7a6b465f24baf64e29c6e65db78
bcrombie <bcrombie09@gmail.com>
2025-07-19 08:23:12 -0400

    Updated app.py and html pages

--- App.py
+++ App.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO
+from main.Numericals import SMROptimizer
 
 app = Flask(__name__, template_folder = "templates")
+# SocketIO used to route and update matplotlib figures
+socketio = SocketIO(app)
 
 @app.route('/assets/<path:filename>')
 def serve_image(filename):
@@ -21,6 +25,15 @@ def methodology():
 @app.route('/profile')
 def account():
     return render_template('account.html')
+
+@socketio.on('start_run')
+def handle_start_algo(data): 
+    # Specify method chosen. Default to gradient descent if selection is invalid
+    method = data.get('method', 'GD')
+    optimizer = SMROptimizer(method = method)
+    # The algorithms will yield the iteration and the filepath.
+    for i, path_name in optimizer.gradientDescent():
+        socketio.emit('update_plot', {'filename': '/' + path_name})
 
 if __name__ == "__main__":
     app.run(debug=True)
